8204ac37c4d7674c02229c3b8343feff9026928f
Dmitriy Kazinov <d.kazinov@drooms.com>
2017-02-08 22:30:31 +0100

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
 
-console.log('here');
 const store = configureStore();
 render(
 <Provider store={store}>
